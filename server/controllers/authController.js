92f982f3f3fd2a4ff5b3b0a8f17b876f5739dc16
farhat-fakhar <farhatbibiva@gmail.com>
2025-08-22 18:26:59 +0500

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,6 +25,7 @@ export const register = async (req, res) => {
       httpOnly: true,
       sameSite: "None",
       secure: process.env.NODE_ENV === "production",
+       path: "/",  
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -78,7 +79,8 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: process.env.NODE_ENV === "None",
+       path: "/",  
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.send({ success: true });
